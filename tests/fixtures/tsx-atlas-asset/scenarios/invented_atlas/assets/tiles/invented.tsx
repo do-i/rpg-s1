1c09ef69f35f2fcd32b9b79ea1fa6208cb4dc88b
Ninja <ninja@renton.net>
2026-08-10 11:21:48 -0400

    Load TSX textures into Bevy atlases

---
+++ tests/fixtures/tsx-atlas-asset/scenarios/invented_atlas/assets/tiles/invented.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" name="invented" tilewidth="2" tileheight="2" tilecount="6" columns="3">
+  <image source="invented.png" width="6" height="4"/>
+</tileset>
